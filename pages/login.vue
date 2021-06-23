b4cc7ace55810421e1a0a80527e1a88df5ce7a66
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2021-06-23 16:06:24 +0100

    finished everything login

--- pages/login.vue
+++ pages/login.vue
@@ -27,9 +27,9 @@
 
         <p class="form-or form-center mb-form">OR</p>
 
-        <div class="gen-wrapper mb-form">
-          <mainbutton :onClick="googleLog" class="ml btn" size="max" mode="outline"><img src="~/assets/svg/google.svg" alt="" /><span>Log in with Google</span></mainbutton>
-        </div>
+        <googleSignIn class="gen-wrapper mb-form" customButton customButtonId='gsignin' :clientId="'156377464736-vlgvsdtk4ka382a36bnb48r2m82afdfd.apps.googleusercontent.com'" :successCallBack="getSuccessData" :failureCallBack="getFailureData">
+          <mainbutton  :loading="gloading" id="gsignin" :onClick="setLoading" class="ml btn" size="max" mode="outline"><img src="~/assets/svg/google.svg" alt="" /><span>Login using Google</span></mainbutton>
+        </googleSignIn>
         
         <p class="form-right mb-form"><span class="form-or">OR </span><nuxt-link to="/signup">Create an account instead</nuxt-link> </p>
       </form>
@@ -46,20 +46,29 @@
 import formlayout from "@/components/layout/formlayout";
 import eye from "@/components/utilities/eye";
 import mainbutton from "@/components/utilities/mainbutton";
+import googleSignIn from 'google-signin-vue/src/googleSignIn.vue';
 
 export default {
   middleware: ['notAuth'],
+  head: {
+    script: [
+      { src: "https://apis.google.com/js/platform.js" },
+    ]
+  },
   components: {
     formlayout,
     eye,
-    mainbutton
+    mainbutton,
+    googleSignIn
   },
   data() {
     return {
       loading: false,
+      gloading: false,
       error: false,
       isLogin: true,
       form: {
+        tokenId: null,
         email: null,
         password: null
       }
@@ -82,9 +91,26 @@ export default {
       })
       .catch(e => console.log(e, 'failed to login'))
     },
-    googleLog() {
-      this.loading = true
-    }
+    getSuccessData(user) {
+      let googleIdToken = user.id_token
+      console.log(googleIdToken);
+      this.form.tokenId = googleIdToken
+      this.$store.dispatch("profile/authenticateUser", {
+        isGoogle: true,
+        form: this.form
+      })
+      .catch(err => {
+        this.gloading = true
+        console.log(err.response)
+      })
+    },
+    getFailureData(errorData) {
+      console.log(errorData);
+      this.gloading = false
+    },
+    setLoading() {
+      this.gloading = true
+    },
   }
 }
 </script>
--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -31,7 +31,7 @@
         <p class="form-or form-center mb-form">OR</p>
 
         <googleSignIn class="gen-wrapper mb-form" customButton customButtonId='gsignin' :clientId="'156377464736-vlgvsdtk4ka382a36bnb48r2m82afdfd.apps.googleusercontent.com'" :successCallBack="getSuccessData" :failureCallBack="getFailureData">
-          <mainbutton  :loading="gloading" id="gsignin" :onClick="googleLog" class="ml btn" size="max" mode="outline"><img src="~/assets/svg/google.svg" alt="" /><span>Sign in using Google</span></mainbutton>
+          <mainbutton  :loading="gloading" id="gsignin" :onClick="setLoading" class="ml btn" size="max" mode="outline"><img src="~/assets/svg/google.svg" alt="" /><span>Sign in using Google</span></mainbutton>
         </googleSignIn>
         
         <p class="form-right mb-form"><span class="form-or">Already have an account? </span><nuxt-link to="/login">Sign in instead</nuxt-link> </p>
@@ -97,19 +97,24 @@ export default {
       })
     },
     getSuccessData(user) {
-      console.log(user);
+      let googleIdToken = user.id_token
+      console.log(googleIdToken);
+      this.form.tokenId = googleIdToken
+      this.$store.dispatch("profile/authenticateUser", {
+        isGoogle: true,
+        form: this.form
+      })
+      .catch(err => {
+        this.gloading = true
+        console.log(err.response)
+      })
     },
     getFailureData(errorData) {
       console.log(errorData);
       this.gloading = false
     },
-    googleLog() {
+    setLoading() {
       this.gloading = true
-
-      // this.$store.dispatch("profile/authenticateUser", {
-      //   isGoogle: true,
-      //   form: this.form
-      // })
     },
     doNothing() {
       return
