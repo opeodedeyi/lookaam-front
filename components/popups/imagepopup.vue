27367c37bf9424d5f10a87278ac6e32cf99327e7
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2021-07-06 13:09:45 +0100

    Update imagepopup.vue

--- components/popups/imagepopup.vue
+++ components/popups/imagepopup.vue
@@ -20,7 +20,15 @@
 import Vue from 'vue'
 import Vue2TouchEvents from 'vue2-touch-events'
 
-Vue.use(Vue2TouchEvents)
+Vue.use(Vue2TouchEvents, {
+    disableClick: false,
+    touchClass: '',
+    tapTolerance: 10,
+    touchHoldTolerance: 400,
+    swipeTolerance: 30,
+    longTapTimeInterval: 400,
+    namespace: 'touch'
+})
 
 export default {
     props: {
